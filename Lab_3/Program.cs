bbfe101d9a24c0a821ab13734b9324e19c98bd71
grush <grush@DESKTOP-TNA0QT8>
2025-04-19 17:35:00 +0300

    Добавьте файлы проекта.

---
+++ Lab_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть ціле число N:");
+        int N = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 1; i <= N; i++)
+        {
+            sum += i * i;
+        }
+
+        Console.WriteLine($"Сума квадратів чисел від 1 до {N} дорівнює: {sum}");
+    }
+}
